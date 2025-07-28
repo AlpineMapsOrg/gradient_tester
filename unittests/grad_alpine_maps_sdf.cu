3c90da4134f9324538280faa6ddcb149a9f2c577
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2025-07-28 13:35:18 +0200

    optimisation / cleanup ++

--- unittests/grad_alpine_maps_sdf.cu
+++ unittests/grad_alpine_maps_sdf.cu
@@ -80,14 +80,10 @@ Vec3 sdf_with_grad(const GeomData& data, const Vec2& uv, Scalar incoming_grad)
     const auto one_over_dot0 = 1 / glm::dot(e0, e0);
     const auto div0 = dot0 * one_over_dot0;
     const Vec2 pq0 = v0 - e0 * glm::clamp(div0, Scalar(0), Scalar(1));
+    const auto dot_pq0_pq0 = glm::dot(pq0, pq0);
 
     Scalar poly_sign = 1.0;
     Scalar distance_sq = 1.0;
-
-    const auto grad_sqrt_V = incoming_grad;
-
-    // no grad for poly_sign
-    const auto grad_distance_sq = grad_sqrt_V;
 
     Vec2 grad_uv = {};
     Vec2 grad_pq0 = {};
@@ -107,7 +103,7 @@ Vec3 sdf_with_grad(const GeomData& data, const Vec2& uv, Scalar incoming_grad)
         const Vec2 pq1 = v1 - e1 * clamp1;
         const Vec2 pq2 = v2 - e2 * clamp2;
         const Scalar s = glm::sign(e0.x * e2.y - e0.y * e2.x);
-        const Vec2 d0 = Vec2(glm::dot(pq0, pq0), s * (v0.x * e0.y - v0.y * e0.x));
+        const Vec2 d0 = Vec2(dot_pq0_pq0, s * (v0.x * e0.y - v0.y * e0.x));
         const Vec2 d1 = Vec2(glm::dot(pq1, pq1), s * (v1.x * e1.y - v1.y * e1.x));
         const Vec2 d2 = Vec2(glm::dot(pq2, pq2), s * (v2.x * e2.y - v2.y * e2.x));
         const Vec2 d = min(min(d0, d1), d2);
@@ -116,59 +112,49 @@ Vec3 sdf_with_grad(const GeomData& data, const Vec2& uv, Scalar incoming_grad)
         distance_sq = d.x;
 
         // gradient computation
-        // result = d.x;
         Scalar grad_d0_x = 0;
         Scalar grad_d1_x = 0;
         Scalar grad_d2_x = 0;
-        // Scalar d = min(min(d0, d1), d2);
+
         if (d0.x <= d1.x && d0.x <= d2.x) {
-            grad_d0_x = grad_distance_sq;
+            grad_d0_x = incoming_grad;
         } else if (d1.x < d0.x && d1.x <= d2.x) {
-            grad_d1_x = grad_distance_sq;
+            grad_d1_x = incoming_grad;
         } else {
             assert(d2.x <= d0.x && d2.x <= d1.x);
-            grad_d2_x = grad_distance_sq;
+            grad_d2_x = incoming_grad;
         }
         grad_pq0 = Scalar(2) * pq0 * grad_d0_x;
         const auto grad_pq1 = Scalar(2) * pq1 * grad_d1_x;
         const auto grad_pq2 = Scalar(2) * pq2 * grad_d2_x;
 
-        // Vec2 pq1 = v1 - e1 * clamp1;
         auto grad_v1 = grad_pq1;
         auto grad_e1 = -grad_pq1 * clamp1;
         const auto grad_clamp1 = -glm::dot(e1, grad_pq1);
 
-        // Vec2 pq2 = v2 - e2 * clamp2;
         auto grad_v2 = grad_pq2;
         auto grad_e2 = -grad_pq2 * clamp2;
         const auto grad_clamp2 = -glm::dot(e2, grad_pq2);
 
-        // const auto clamp1 = glm::clamp(div1, Scalar(0), Scalar(1));
         const auto grad_div1 = stroke::grad::clamp(div1, Scalar(0), Scalar(1), grad_clamp1);
 
-        // const auto clamp2 = glm::clamp(div2, Scalar(0), Scalar(1));
         const auto grad_div2 = stroke::grad::clamp(div2, Scalar(0), Scalar(1), grad_clamp2);
 
-        // const auto div1 = dot1 * one_over_dot1;
         const auto grad_dot1 = grad_div1 * one_over_dot1;
 
-        // const auto div2 = dot2 * one_over_dot2;
         const auto grad_dot2 = grad_div2 * one_over_dot2;
 
-        // const auto dot1 = glm::dot(v1, e1);
         grad_v1 += e1 * grad_dot1;
         grad_e1 += v1 * grad_dot1;
-        // const auto dot2 = glm::dot(v2, e2);
+
         grad_v2 += e2 * grad_dot2;
         grad_e2 += v2 * grad_dot2;
 
-        // continue with
         grad_uv += grad_v1 + grad_v2;
 
     } else {
-        // result = dot(pq0, pq0);
-        grad_pq0 += Scalar(2) * pq0 * grad_distance_sq;
-        distance_sq = dot(pq0, pq0);
+        grad_pq0 += Scalar(2) * pq0 * incoming_grad;
+        distance_sq = dot_pq0_pq0;
     }
 
     Vec2 grad_v0 = grad_pq0;
